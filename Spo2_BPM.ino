4e8a619ff8c9c8de08f9c77252be6717ca5325d3
Chandra Bhusan Rai <86246296+cbrai@users.noreply.github.com>
2022-02-25 20:57:52 +0530

    Create Spo2_BPM.ino

---
+++ Spo2_BPM.ino
@@ -0,0 +1,167 @@
+#include <Wire.h>
+#include "MAX30100_PulseOximeter.h"
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+ 
+#define REPORTING_PERIOD_MS     1000
+ 
+PulseOximeter pox;
+uint32_t tsLastReport = 0;
+
+float BPM, SpO2; 
+const char *ssid = "Believer"; //Enter your WIFI ssid
+const char *password = "61984912"; //Enter your WIFI password
+ESP8266WebServer server(80);
+void onBeatDetected()
+{
+    Serial.println("Beat!");
+}
+ 
+void setup()
+{
+    Serial.begin(115200);
+      //connect to your local wi-fi network
+    Serial.println("Connecting to ");
+    Serial.println(ssid);
+    
+    WiFi.begin(ssid, password);
+ 
+    //check wi-fi is connected to wi-fi network
+    while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected..!");
+    Serial.print("Got IP: ");  Serial.println(WiFi.localIP());
+ 
+    server.on("/", handle_OnConnect);
+    server.onNotFound(handle_NotFound);
+ 
+    server.begin();
+    Serial.println("HTTP server started");
+    Serial.print("Initializing pulse oximeter..");
+ 
+    // Initialize the PulseOximeter instance
+    // Failures are generally due to an improper I2C wiring, missing power supply
+    // or wrong target chip
+    
+    if (!pox.begin()) {
+        Serial.println("FAILED");
+        for(;;);
+    } else {
+        Serial.println("SUCCESS");
+        pox.setOnBeatDetectedCallback(onBeatDetected);
+    }
+     pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
+ 
+    // Register a callback for the beat detection
+    
+}
+ 
+void loop()
+{
+    server.handleClient();
+    // Make sure to call update as fast as possible
+    pox.update();
+    BPM = pox.getHeartRate();
+    SpO2 = pox.getSpO2();
+    if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
+        Serial.print("Heart rate:");
+        Serial.print(BPM);
+        Serial.print("bpm / SpO2:");
+        Serial.print(SpO2);
+        Serial.println("%");
+ 
+        tsLastReport = millis();
+    }
+}
+
+void handle_OnConnect() {
+  
+  server.send(200, "text/html", SendHTML(BPM, SpO2)); 
+}
+ 
+void handle_NotFound(){
+  server.send(404, "text/plain", "Not found");
+}
+
+String SendHTML(float BPM,float SpO2){
+String html = "<!DOCTYPE html>";
+html += "<html>";
+html += "<head>";
+html += "<title>Health Monitoring System</title>";
+html += "<meta name='viewport' content='width=device-width, initial-scale=1.0'>";
+html += "<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/all.min.css'>";
+html += "<link rel='stylesheet' type='text/css' href='styles.css'>";
+html += "<style>";
+html += "body { background-color: #fff; font-family: sans-serif; color: #333333; font: 14px Helvetica, sans-serif box-sizing: border-box;}";
+html += "#page { margin: 20px; background-color: #fff;}";
+html += ".container { height: inherit; padding-bottom: 20px;}";
+html += ".header { padding: 20px;}";
+html += ".header h1 { padding-bottom: 0.3em; color: #008080; font-size: 45px; font-weight: bold; font-family: Garmond, 'sans-serif'; text-align: center;}";
+html += "h2 { padding-bottom: 0.2em; border-bottom: 1px solid #eee; margin: 2px; text-align: left;}";
+html += ".header h3 { font-weight: bold; font-family: Arial, 'sans-serif'; font-size: 17px; color: #b6b6b6; text-align: center;}";
+html += ".box-full { padding: 20px; border 1px solid #ddd; border-radius: 1em 1em 1em 1em; box-shadow: 1px 7px 7px 1px rgba(0,0,0,0.4); background: #fff; margin: 20px; width: 300px;}";
+html += "@media (max-width: 494px) { #page { width: inherit; margin: 5px auto; } #content { padding: 1px;} .box-full { margin: 8px 8px 12px 8px; padding: 10px; width: inherit;; float: none; } }";
+html += "@media (min-width: 494px) and (max-width: 980px) { #page { width: 465px; margin 0 auto; } .box-full { width: 380px; } }";
+html += "@media (min-width: 980px) { #page { width: 930px; margin: auto; } }";
+html += ".sensor { margin: 12px 0px; font-size: 2.5rem;}";
+html += ".sensor-labels { font-size: 1rem; vertical-align: middle; padding-bottom: 15px;}";
+html += ".units { font-size: 1.2rem;}";
+html += "hr { height: 1px; color: #eee; background-color: #eee; border: none;}";
+html += "</style>";
+
+//Ajax Code Start
+  html += "<script>\n";
+  html += "setInterval(loadDoc,1000);\n";
+  html += "function loadDoc() {\n";
+  html += "var xhttp = new XMLHttpRequest();\n";
+  html += "xhttp.onreadystatechange = function() {\n";
+  html += "if (this.readyState == 4 && this.status == 200) {\n";
+  html += "document.body.innerHTML =this.responseText}\n";
+  html += "};\n";
+  html += "xhttp.open(\"GET\", \"/\", true);\n";
+  html += "xhttp.send();\n";
+  html += "}\n";
+  html += "</script>\n";
+  //Ajax Code END
+  
+html += "</head>";
+html += "<body>";
+html += "<div id='page'>";
+html += "<div class='header'>";
+html += "<h1>Spo2 and HeartRate</h1>";
+html += "</div>";
+html += "<div id='content' align='center'>";
+html += "<div class='box-full' align='left'>";
+html += "<div class='sensors-container'>";
+
+//For Heart Rate
+html += "<p class='sensor'>";
+html += "<i class='fas fa-heartbeat' style='color:#cc3300'></i>";
+html += "<span class='sensor-labels'> Heart Rate </span>";
+html += (int)BPM;
+html += "<sup class='units'>BPM</sup>";
+html += "</p>";
+html += "<hr>";
+
+//For Sp02
+html += "<p class='sensor'>";
+html += "<i class='fas fa-burn' style='color:#f7347a'></i>";
+html += "<span class='sensor-labels'> Sp02 </span>";
+html += (int)SpO2;
+html += "<sup class='units'>%</sup>";
+html += "</p>";
+html += "<hr>";
+html += "</div>";
+html += "</div>";
+html += "</div>";
+html += "</div>";
+html += "</div>";
+html += "</body>";
+html += "</html>";
+
+return html;
+}
